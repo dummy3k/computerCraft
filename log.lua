a84cedf9911c74c16fa83341688f9dcc8c40934f
dummy3k <l4711@gmx.net>
2013-05-24 20:49:52 +0200

    log.lua

---
+++ log.lua
@@ -0,0 +1,10 @@
+function debug(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10)
+
+    print(os.day(),":",os.time()," ",s1, s2, s3, s4, s5, s6, s7, s8, s9, s10)
+
+end
+function info(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10)
+
+    print(os.day(),":",os.time()," ",s1, s2, s3, s4, s5, s6, s7, s8, s9, s10)
+
+end
