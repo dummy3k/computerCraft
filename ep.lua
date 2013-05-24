cd1e43619d0f62f4a3e672cdf18202b6c60eb399
dummy3k <l4711@gmx.net>
2013-05-24 19:18:07 +0200

    ep: download and run without parameters

--- ep.lua
+++ ep.lua
@@ -1,18 +1,15 @@
--- http://beta.etherpad.org/p/Dmw3wsX4tH/export/txt
+print("version: 6")
 function get(name, save_as)
-	local url = "http://beta.etherpad.org/p/"..name.."/export/txt"
-	local content = http.get(url)
-	if not content then error("download failed: "..url) end
-	print(content)
-
-	if not save_as then save_as = name end
-	f = io.open(save_as, "w")
-	f:write(content.readAll())
-	f:close()
-
+	-- http://beta.etherpad.org/p/Dmw3wsX4tH/export/txt
+    local url = "http://beta.etherpad.org/p/"..name.."/export/txt"
+    print(url)
+    local content = http.get(url)
+    if not content then error("download failed: "..url) end
+    if not save_as then save_as = name end
+    f = io.open(save_as, "w")
+    f:write(content.readAll())
+    f:close()
 end
-
 local tArgs = {...}
 get(tArgs[1], tArgs[2])
-
-
+os.run({}, tArgs[2])
