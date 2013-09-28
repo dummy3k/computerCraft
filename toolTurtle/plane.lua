53af6b755379698fa9f89acd7bd68c1786fc6662
dummy3k <l4711@gmx.net>
2013-09-28 16:41:52 +0200

    plane with more then one stack

--- toolTurtle/plane.lua
+++ toolTurtle/plane.lua
@@ -4,8 +4,14 @@ local tArgs = {...}
 width = tArgs[1]
 height = tArgs[2]
 
+local current_slot = 1
+fox.select(current_slot)
+
 for w=1,width do
 	for h=1,height do
+		while fox.getItemCount(current_slot) == 0 and current_slot <= 16 do
+			fox.select(current_slot)
+		end
 		fox.placeDown("false")
 		fox.forward()
 	end
