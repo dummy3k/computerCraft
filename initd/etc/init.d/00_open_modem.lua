b5f9cc16916a0eea915ced61b65d490a68faec7a
dummy3k <l4711@gmx.net>
2015-08-23 18:22:11 +0200

    initd/etc/init.d/00_open_modem.lua: test all sides

--- initd/etc/init.d/00_open_modem.lua
+++ initd/etc/init.d/00_open_modem.lua
@@ -1,6 +1,18 @@
-if peripheral.getType("right") == "modem" then
-	rednet.open("right")
-	print("modem open")
-else
-	print("no modem found")
+sides = {"left", "right", "front", "back", "top", "bottom"}
+
+for _, side in ipairs(sides) do
+	if peripheral.getType(side) == "modem" then
+		rednet.open(side)
+		print("modem open ("..side..")")
+	end
 end
+
+-- if peripheral.getType("right") == "modem" then
+	-- rednet.open("right")
+	-- print("modem open (right)")
+-- if peripheral.getType("right") == "modem" then
+	-- rednet.open("right")
+	-- print("modem open (right)")
+-- else
+	-- print("no modem found")
+-- end
